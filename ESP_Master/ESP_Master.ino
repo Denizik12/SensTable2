4a8ca6ec33c1b76701a7f0db7609ba7ba5b36f38
ZiweZhang <58477461+ZiweZhang@users.noreply.github.com>
2021-03-24 00:10:15 +0100

    Arduino code

---
+++ ESP_Master/ESP_Master.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+// Netwerk Configuratie
+char * ssid_ap = "ESP8266_SSID";
+char * password_ap = "test1234"; //minimaal 8 characters
+
+// Server setup
+ESP8266WebServer server(80);
+IPAddress ip(192,168,11,4); // arbitrary IP address (doesn't conflict w/ local network)
+IPAddress gateway(192,168,11,1);
+IPAddress subnet(255,255,255,0);
+
+String sensor_type = "";
+float sensor_value = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid_ap, password_ap);
+  Serial.print("Setting AP (Access Point)…");
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.println();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", handleRoot);
+  server.on("/update", handleUpdate);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "Sensor: " + String(sensor_type) + " Value: " + String(sensor_value));
+}
+
+void handleUpdate() {
+  // The value will be passed as a URL argument
+  sensor_type = server.arg("type");
+  sensor_value = server.arg("value").toFloat();
+  Serial.print(sensor_type); Serial.print("   "); Serial.println(sensor_value);
+  server.send(200, "text/plain", "Updated");
+}
---
+++ ESP_Slaves/ESP_Slaves.ino
@@ -0,0 +1,50 @@
+#include <ESP8266WiFi.h>
+
+// Netwerk configuratie
+const char* ssid = "ESP8266_SSID";
+const char* password = "test1234";
+const char* host = "192.168.11.4";
+
+// Client object
+WiFiClient client;
+
+// Tijd tussen metingen
+const int sleepTimeSeconds = 2;
+
+String sensor_type = "temperature";
+float sensor_value = 28.2;
+
+void setup() {
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+
+  // wacht totdat er een WiFi connectie is
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("IP Address (AP): "); Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  if(client.connect(host,80)) {
+    String url = "/update?type=";
+    url += String(sensor_type);
+    url += "&value=";
+    url += String(sensor_value);
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host +  "\r\n" + 
+                 "Connection: keep-alive\r\n\r\n"); // minimum set of required URL headers
+    delay(10);
+    // Read all the lines of the response and print them to Serial
+    // Alleen gebruiken om connectie te testen
+/*    Serial.println("Response: ");
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    Serial.println();  */
+  }
+  delay(2000);
+}
