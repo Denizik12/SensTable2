091a2b87c36d5c5e45e59a42322cd49280696ae5
ZiweZhang <58477461+ZiweZhang@users.noreply.github.com>
2021-03-25 12:18:53 +0100

    .

--- Sensor code/Ultrasoon/Ultrasoon.ino
+++ Sensor code/Ultrasoon/Ultrasoon.ino
@@ -11,17 +11,26 @@ WiFiClient client;
 // Tijd tussen metingen
 const int sleepTimeSeconds = 2;
 
-String sensor_type = "Reed";
+String sensor_type = "Ultrasoon";
 float sensor_value = 0;
 
-#define sensor_pin 16  //D0
+#define trigPin 16 //D0
+#define echoPin 5  //D1
 
 void readSensor() {
-  sensor_value = (float)digitalRead(sensor_pin);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  sensor_value = (pulseIn(echoPin, HIGH)) * 0.0343 / 2;
 }
 
 void setupSensor() {
-  pinMode(sensor_pin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void setup() {
@@ -39,22 +48,23 @@ void setup() {
 }
 
 void loop() {
-  if(client.connect(host,80)) {
+
+  if (client.connect(host, 80)) {
     String url = "/update?type=";
     url += String(sensor_type);
     url += "&value=";
     url += String(sensor_value);
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host +  "\r\n" + 
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host +  "\r\n" +
                  "Connection: keep-alive\r\n\r\n"); // minimum set of required URL headers
     delay(10);
     // Read all the lines of the response and print them to Serial
     // Alleen gebruiken om connectie te testen
-/*    Serial.println("Response: ");
-    while(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-    Serial.println();  */
+    /*    Serial.println("Response: ");
+        while(client.available()){
+          String line = client.readStringUntil('\r');
+          Serial.print(line);
+        }
+        Serial.println();  */
   }
   delay(2000);
 }
