51d68464aac506ca56405b8a7ffdb0575c60b6a0
ZiweZhang <58477461+ZiweZhang@users.noreply.github.com>
2021-03-25 12:05:47 +0100

    Update ESP_Slaves.ino

--- ESP_Slaves/ESP_Slaves.ino
+++ ESP_Slaves/ESP_Slaves.ino
@@ -11,12 +11,23 @@ WiFiClient client;
 // Tijd tussen metingen
 const int sleepTimeSeconds = 2;
 
-String sensor_type = "temperature";
-float sensor_value = 28.2;
+String sensor_type = "Reed";
+float sensor_value = 0;
+
+#define sensor_pin 16
+
+void readSensor() {
+  sensor_value = (float)digitalRead(sensor_pin);
+}
+
+void setupSensor() {
+  pinMode(sensor_pin, OUTPUT);
+}
 
 void setup() {
   WiFi.begin(ssid, password);
   Serial.begin(115200);
+  setupSensor();
 
   // wacht totdat er een WiFi connectie is
   while (WiFi.status() != WL_CONNECTED) {
@@ -28,7 +39,6 @@ void setup() {
 }
 
 void loop() {
-
   if(client.connect(host,80)) {
     String url = "/update?type=";
     url += String(sensor_type);
