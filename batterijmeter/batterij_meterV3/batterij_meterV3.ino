8ad83ca25cb23a2bf2cacbec78d84134bae8c547
Deniz <denizik12@hotmail.com>
2021-06-26 17:18:59 +0200

    Charger added to powerbank

--- batterijmeter/batterij_meterV3/batterij_meterV3.ino
+++ batterijmeter/batterij_meterV3/batterij_meterV3.ino
@@ -13,43 +13,44 @@ float voltage = 0.0;    // Voltage
 int percentageCalculation = 0;    // Percentage calculation formula
 int percentage = 0;    // Percentage based on the voltage
 
-String batteryMessage = "Geen batterij";
+String batteryMessage = "Geen batterij";    // Default message
 
-int chargePin = 2;
+#define CHARGE_PIN 3    // Charger pin
+#define LED_PIN 2    // Pin for led
 
 void setup() {
-  Serial.begin(9600);    // Begin the serial monitor
-  pinMode(chargePin, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(CHARGE_PIN, INPUT);
   lcd.init();    // Initialize the lcd
 }
 
 void loop() {
 
-    if (digitalRead(chargePin) == HIGH) {
-      lcd.backlight();
-      lcd.setCursor(0, 0);
-      lcd.print("Aan het opladen ");
-      lcd.setCursor(0, 1);
-      lcd.print("                ");
-      //Serial.println("HIGH");
-    } else {
-      //Serial.println("LOW");
-      refreshDisplay();    // Print the values on the lcd
-      sampleCount = 0;
-      sum = 0;
-    }
+  // Check if the powerbank is charging
+  if (digitalRead(CHARGE_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Aan het opladen ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    refreshDisplay();    // Print the values on the lcd
+    sampleCount = 0;
+    sum = 0;
+  }
 }
 
 float volt() {
-  // Take a number of the analog samples and add them up
+  // Take the numbers of the analog samples and add them up
   while (sampleCount < NUM_SAMPLES) {
     sum += analogRead(A2);
     sampleCount++;
     delay(10);
   }
   voltage = ((float)sum / (float)NUM_SAMPLES * 5.0) / 1024.0;
-  //float result = voltage / (100000/1100000); // (R2 / (R1 + R2))
-  float result = voltage * 11.5;
+  float result = voltage * 11.5;  // Input voltage to real voltage of the powerbank
   return result;
 }
 
@@ -58,7 +59,7 @@ int percentageBattery(float voltage) {
 
   // Return the correct value for the percentage
   if (voltage >= MAX_VOLTAGE) {
-    percentage = 99;
+    percentage = 99;  // Safety for lcd screen
   } else if (voltage < MIN_VOLTAGE) {
     percentage = 0;
   } else {
@@ -70,7 +71,7 @@ int percentageBattery(float voltage) {
 String bericht(int percentage) {
   // Check the percentage and give the correct message
   if (percentage >= 50) {
-    batteryMessage = "Genoeg stroom";
+    batteryMessage = "Genoeg spanning";
   } else if (percentage >= 20) {
     batteryMessage = "  Bijna leeg ";
   } else {
@@ -93,18 +94,11 @@ void lcdDisplay() {
   lcd.print(bericht(percentageBattery(volt())));
 }
 
-void lcdClear() {
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("                ");
-  lcd.setCursor(0, 1);
-  lcd.print("                ");
-}
-
+// Refresh the values on the lcd
 void refreshDisplay() {
   unsigned long currentTime = millis();
   static unsigned long previousTime = 0;
-  const unsigned long interval = 500; //    Every 1 minute
+  const unsigned long interval = 1000; //    Every 1 minute
   if (currentTime - previousTime >= interval) {
     previousTime += interval;
     lcdDisplay();
